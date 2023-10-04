dc17b0bc78d5d948b8739c9d5893eb88a9559680
Narmus <narmusgaming@gmail.com>
2023-10-04 19:18:33 +0530

    Post Page - Input boxes completed

--- src/Containers/PostPage/views/PostPage.jsx
+++ src/Containers/PostPage/views/PostPage.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { Fragment } from "react";
+import "./PostPage.css";
 
 const PostPageView = () => {
-  return <div>Posts here</div>;
+  return (
+    <div className="post-page">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+          <input
+            placeholder="Type your question here..."
+            className="question-box"
+          ></input>
+          <textarea placeholder="Description..." className="question-description-box"></textarea>
+        <div className="post-area">
+          <button className="question-post-btn" type="submit">
+            Post
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default PostPageView;
